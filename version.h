a4b9fa5b3e76265b4da033e84bb0ec894c3d4bbd
Igrek <igrek.s@o2.pl>
2015-08-29 12:00:27 +0200

    dupa

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
-// -- 20:17:36, 28.08.2015 -- (ymake v1.5.77) --
+// -- 20:21:17, 28.08.2015 -- (ymake v1.5.77) --
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "1.5.79"
+#define VERSION "1.5.81"
 
 #endif
